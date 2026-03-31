5b318b0d0034e7a0903c19a2ccff326e407901cd
Edwar Stheven <edwarsthat@gmail.com>
2026-03-31 15:17:48 -0500

    se corrigio los cors para produccion

--- src/app/app.rs
+++ src/app/app.rs
@@ -15,7 +15,7 @@ pub fn create_router(db_pool: MongoDb) -> Router {
     let state = AppState { db: db_pool };
 
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+        .allow_origin("https://credenciales.celifrut.com".parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE, Method::OPTIONS])
         .allow_headers([header::CONTENT_TYPE, header::AUTHORIZATION, header::ACCEPT])
         .allow_credentials(true);
